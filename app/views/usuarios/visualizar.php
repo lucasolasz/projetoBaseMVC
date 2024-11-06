430fc11c968cc1b437f0fbe9dcca18a56372855e
lucasolasz <olaszlucas072@gmail.com>
2024-11-05 23:07:31 -0300

    Inclui DataTables com processamento pelo servidor

--- app/views/usuarios/visualizar.php
+++ app/views/usuarios/visualizar.php
@@ -15,17 +15,18 @@
 
             <h5 class="tituloIndex">Usuários
                 <div style="float: right;">
-                    <a href="<?= URL . 'Usuarios/cadastrar' ?>" class="btn btn-primary">Novo usuário</a>
+                    <a href="<?= URL . 'Usuarios/cadastrar' ?>" class="btn btn-primary">Novo</a>
                 </div>
             </h5>
 
         </div>
         <div class="card-body">
             <div class="table-responsive">
-                <table id="tableDataTablePtBr" class="table table-hover">
+                <table id="tabela" class="table table-hover">
                     <thead>
                         <tr>
                             <th scope="col">Nome usuário</th>
+                            <th scope="col">Cargo</th>
                             <th scope="col">Ações</th>
                         </tr>
                     </thead>
@@ -34,14 +35,50 @@
                             <tr>
                                 <td><?= ucfirst($usuarios->ds_nome_usuario) ?></td>
                                 <td>
-                                    <a href="<?= URL . 'Usuarios/editarUsuario/' . $usuarios->id_usuario ?>" class="btn btn-warning"><i class="bi bi-pencil-square"></i> Editar</a>
+                                    <a href="<?= URL . 'Usuarios/editarUsuario/' . $usuarios->id_usuario ?>"
+                                        class="btn btn-warning"><i class="bi bi-pencil-square"></i> Editar</a>
 
-                                    <a href="<?= URL . 'Usuarios/deletarUsuario/' . $usuarios->id_usuario ?>" class="btn btn-danger"><i class="bi bi-trash-fill"></i> Exlcuir</a>
+                                    <a href="<?= URL . 'Usuarios/deletarUsuario/' . $usuarios->id_usuario ?>"
+                                        class="btn btn-danger"><i class="bi bi-trash-fill"></i> Exlcuir</a>
                                 </td>
-                            <?php  } ?>
+                            <?php } ?>
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
 </div>
+<script>
+    var params = {
+        tabela: 'tb_usuario', //tabela que será utilizada para pesquisa
+        colunas_pesquisa: ['ds_nome_usuario'], //coluna para a busca dentro da tabela. Input Search
+        colunas_ordenacao: ['ds_nome_usuario'], //colunas utilizadas para ordenação
+        joins: [
+            {
+                tabela: 'tb_cargo',
+                condicao: 'tb_usuario.fk_cargo = tb_cargo.id_cargo'
+            }
+        ], // Joins se necessário
+        columns: [
+            { "data": "ds_nome_usuario" },
+            { "data": "ds_cargo" },
+            {
+                "data": null, // Define como null pois será preenchido manualmente
+                "orderable": false, // Impede ordenação para esta coluna
+                "render": function (data, type, row) {
+                    // Retorna o HTML para os botões de ação, com href dinâmico
+                    return `
+                    <a href="/Usuarios/editarUsuario/${row.id_usuario}" class="btn btn-warning">
+                        <i class="bi bi-pencil-square"></i> Editar
+                    </a>
+                    <a href="/Usuarios/deletarUsuario/${row.id_usuario}" class="btn btn-danger">
+                        <i class="bi bi-trash-fill"></i> Excluir
+                    </a>`;
+                }
+            }
+        ] // Colunas que irão se adequar as colunas definidas no html
+    };
+
+    // Chama a função passando o ID da tabela e os parâmetros configurados
+    initDataTable('tabela', params);
+</script>
